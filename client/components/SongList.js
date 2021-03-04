5a42a2a93fa76a0162198f569869ec735814c2d5
박도희 <dohee.pa@gmail.com>
2021-03-04 09:11:54 +0900

    42. Fixing Key Warnings

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -3,10 +3,29 @@ import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 
 class SongList extends Component {
+  renderSongs() {
+    return this.props.data.songs.map(song => {
+      return (
+        <li key={song.id} className="collection-item">
+          {song.title}
+        </li>
+      )
+    })
+  }
+
   render() {
-    console.log(this.props);
+    if (this.props.data.loading) {
+      return (
+        <div>loading</div>
+      )
+    }
     return (
-      <div>SongList</div>
+      <div>
+        <h1>SongList</h1>
+        <ul className="collection">
+          {this.renderSongs()}
+        </ul>
+      </div>
     )
   }
 }
@@ -14,6 +33,7 @@ class SongList extends Component {
 const query = gql`
   {
     songs {
+      id
       title
     }
   }
