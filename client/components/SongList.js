4efbeb9724094e5ae6bdcdbd95ba8b54167f4739
박도희 <dohee.pa@gmail.com>
2021-03-05 08:55:31 +0900

    49. Navigating on Successful Mutation

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
 
 class SongList extends Component {
   renderSongs() {
@@ -25,6 +26,11 @@ class SongList extends Component {
         <ul className="collection">
           {this.renderSongs()}
         </ul>
+        <Link
+          to="/songs/new"
+          className="btn-floating btn-large red right">
+          <i className="material-icons">add</i>
+        </Link>
       </div>
     )
   }
